278393ef594a833420b1a59015ffd6ac0871b4aa
Noor-Nizar <noornizaredu@gmail.com>
2021-07-28 14:13:47 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    
+    int n = 0,dist,fuel;
+    cin >> dist;
+    cin >> fuel;
+    cin >> n;
+    vector<int> arr(n);
+    arr.push_back(0);
+    for (int i = 1; i <= n; i++)
+        cin >>  arr[i];
+
+    arr.push_back(dist);n+=2;
+    int i = 1;int refs = 0;
+    while(i < arr.size()){
+        while(arr[i] - arr[0] < fuel && i < arr.size()){
+            i++;
+        }
+        refs += i < arr.size() ? 1 : 0;
+        arr.erase(arr.begin(),arr.begin()+i);
+        i = 1;
+    }
+    cout << refs;
     return 0;
 }
